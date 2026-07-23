fa7605d0c5a37c40ff2d366e0738025ade6de5a0
Yugta Saini <yugtasaini@gmail.com>
2026-07-23 18:46:36 +0530

    Update authService.js

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api/auth",
+  baseURL: "https://career-genie-backend-gf7z.onrender.com/api/auth",
 });
 
 export const registerUser = (data) => {
