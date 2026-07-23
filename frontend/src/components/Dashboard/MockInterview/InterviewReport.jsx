860a111afb3514ce45b1c9e810d4ff26aa1a6ad7
Yugta Saini <yugtasaini@gmail.com>
2026-07-23 19:23:45 +0530

    Update InterviewReport.jsx

--- frontend/src/components/Dashboard/MockInterview/InterviewReport.jsx
+++ frontend/src/components/Dashboard/MockInterview/InterviewReport.jsx
@@ -37,7 +37,7 @@ export default function InterviewReport() {
     loadedRef.current = true;
 
     loadReport();
-
+// eslint-disable-next-line react-hooks/exhaustive-deps
 }, []);
 
     const loadReport = async () => {
